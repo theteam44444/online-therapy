0726cfdf1467c722ad3811d63b9c4749ea97e1f4
Harry Shah <harvinder.shah@gmail.com>
2021-09-23 12:30:09 -0400

    doctor model added

--- models/index.js
+++ models/index.js
@@ -1,6 +1,8 @@
 //import all models
 const Appointment = require("./Appointment");
+const Doctor = require('./Doctor');
+const Client = require('./Client');
 
 //Create associations
 
-module.exports = { Appointment };
+module.exports = { Appointment, Doctor, Client };
