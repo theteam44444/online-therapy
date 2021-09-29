01d6fb457ce2371c85526d9933cc47fdcc3e7176
Harry Shah <harvinder.shah@gmail.com>
2021-09-28 20:31:24 -0400

    patient dashboard handler updated

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,3 +1,5 @@
+const { Appointment, Client } = require("../models");
+
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
@@ -24,16 +26,92 @@ router.get("/about", (req, res) => {
   res.render("about");
 });
 
-router.get("/patient-dashboard", (req, res) => {
-  if (req.session.loggedIn) {
-    res.redirect("/");
-    return;
-  }
-  res.render("patient-dashboard");
-});
+// router.get("/patient-dashboard", (req, res) => {
+//   if (req.session.loggedIn) {
+//     res.redirect("/");
+//     return;
+//   }
+//   res.render("patient-dashboard");
+// });
 
+router.get("/patient-dashboard", async (req, res) => {
+  const appointments = await Appointment.findAll({
+
+    attributes: [
+      'id',
+      'details',
+      'doctor_name',
+      'appointment_date',
+      'appointment_type'
+    ]
+  })
+  .then(dbAppointmentData => {
+    const appointments = dbAppointmentData.map(appointment => appointment.get({ plain: true }));
+    // console.log("appointments", appointments);
+    res.render("patient-dashboard", 
+    {appointments});
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+  console.log("appointments", appointments);
+ });
 // router.get("/doctors", (req, res) => {
 //   res.render("doctor-dashboard");
 // });
+//  get the logged in client information
+router.get("/patient-dashboard", async (req, res) => {
+  const client = await Client.findOne({
+     where: {
+    email: req.session.email
 
+     },
+    attributes: [
+      'first_name',
+      'last_name',
+      'address',
+      'birth_date',
+      'email'
+    ]
+  })
+  .then(dbClientData => {
+    const client = dbClientData.get({ plain: true });
+    console.log("client", client);
+    res.render("patient-dashboard", 
+    {client});
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+  
+});
+
+// router.get("/patient-dashboard", (req, res) => {
+//   Client.findOne({
+//      where: {
+//     email: req.session.email
+
+//      },
+//     attributes: [
+//       'first_name',
+//       'last_name',
+//       'address',
+//       'birth_date',
+//       'email'
+//     ]
+//   })
+//   .then(dbClientData => {
+//     const client = dbClientData.get({ plain: true });
+//     console.log("client", client);
+//     res.render("patient-dashboard", 
+//     {client});
+//   })
+//   .catch(err => {
+//     console.log(err);
+//     res.status(500).json(err);
+//   });
+  
+// });
 module.exports = router;
